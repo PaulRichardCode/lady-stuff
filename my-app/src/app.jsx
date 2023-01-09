cbd0d3768007b5d6e328e3319171fe8eb551b9d5
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-01-09 21:56:34 +0100

    cool

--- my-app/src/app.jsx
+++ my-app/src/app.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "./header.jsx";
-
+import Footer from "./footer.jsx";
+import Image from "./image.jsx";
 export default function MyApp() {
   return (
     <div>
       <Header />
+      <Footer />
+      <Image />
     </div>
   );
 }
---
+++ my-app/src/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <h1>This is the Title</h1>
+      <p>This is the content</p>
+    </div>
+  );
+}
