9206c5374ced31398a9a7fe30e34de0a8dd1f025
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-01-09 19:36:06 +0100

    why

---
+++ keeper-app-part-1-starting/src/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>This is a Header</h1>
+      <p>Hi there want to see my stuffs</p>
+      okay latter
+    </div>
+  );
+}
