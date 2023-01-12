ba06527748cef438027564a4f4d14c99bd8ea6a6
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-01-12 20:57:11 +0100

    kk

--- my-app/src/app.jsx
+++ my-app/src/app.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import Header from "./header.jsx";
 import Footer from "./footer.jsx";
 import Image from "./image.jsx";
-import Contacts from "./contact.js";
-
 export default function MyApp() {
   return (
     <div>
       <Header />
       <Footer />
-      {/*  <Image name="Paul Richard" age="15" place="nigeria" />
+      <Image name="Paul Richard" age="15" place="nigeria" />
       <Image name="chilwell" age="28" place="England" />
       <Image name="felix" age="25" place="togo" />
-      <Image name="felix" age="25" place="togo" /> */}
+      <Image name="felix" age="25" place="togo" />
     </div>
   );
 }
