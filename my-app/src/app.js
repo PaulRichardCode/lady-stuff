60652d751b32b4a09c5d7595732acab00f77cf76
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-01-14 10:09:32 +0100

    okay cool

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,23 +1,8 @@
 import React from "react";
-import Contact from "./contact.js";
-import Joke from "./joke";
 
 export default function MyApp() {
-  const contactElement = Contact.map((joke) => {
-    return <Joke setup={joke.setup} punchline={joke.punchline} />;
-  });
   return <div>{contactElement}</div>;
 }
-
-/*  const colors = [
-    <h3>red</h3>,
-    <h3>blue</h3>,
-    <h3>green</h3>,
-    <h3>orange</h3>,
-    <h3>blue</h3>,
-    <h3>purple</h3>,
-  ];
-<Image name="Paul Richard" age="15" place="nigeria" />
-      <Image name="chilwell" age="28" place="England" />
-      <Image name="felix" age="25" place="togo" />
-      <Image name="felix" age="25" place="togo" />s */
+/* const contactElement = Contact.map((joke) => {
+    return <Joke setup={joke.setup} punchline={joke.punchline} />;
+  }); */
