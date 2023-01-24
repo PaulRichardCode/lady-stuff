49fcf38024a219592f60319474ae0752f755ee51
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-01-24 10:33:39 +0100

    rolla

---
+++ my-app/src/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MountNav from "./MOUNT FUJI/mNav.js";
+import Body from "./MOUNT FUJI/body.jsx";
+
+export default function MyApp() {
+  return (
+    <div>
+      <MountNav />
+      <Body />
+    </div>
+  );
+}
