f0393a4670f39adf0c09055d0c0a67f3e821c1d6
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-02-08 08:12:18 +0100

    ass

--- my-app/src/Classwork/app.js
+++ my-app/src/Classwork/app.js
@@ -3,17 +3,25 @@ import React from "react";
 export default function App() {
   const [value, func] = React.useState(0);
 
-  function math() {
-    func(value++);
+  function add() {
+    func(function (plus) {
+      return plus + 1;
+    });
+  }
+
+  function minus() {
+    func((substract) => substract - 1);
   }
 
   return (
     <div className="main">
-      <button className="counter--minus">-</button>
+      <button className="counter--minus" onClick={minus}>
+        -
+      </button>
       <div className="counter--count">
         <h1>{value}</h1>
       </div>
-      <button className="counter--plus " onClick={math}>
+      <button className="counter--plus " onClick={add}>
         +
       </button>
     </div>
