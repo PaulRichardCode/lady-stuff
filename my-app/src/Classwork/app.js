4d28f67de1e0b0bd7b6433a7bb144748e02bfcb4
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-02-11 09:21:19 +0100

    kk

--- my-app/src/Classwork/app.js
+++ my-app/src/Classwork/app.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function App() {
-  const [isGoingOut, setIsGoingOut] = React.useState(true);
+  /*   const [isGoingOut, setIsGoingOut] = React.useState(true);
 
   function click() {
     setIsGoingOut((going) => !going);
+  }
+ */
+
+  const [isGoingOut, setIsGoingOut] = React.useState(true);
+
+  function click() {
+    setIsGoingOut((going) => (going ? false : true));
   }
 
   return (
     <div className="main">
       <h1>Do you feel like going out</h1>
       <div className="div" onClick={click}>
-        <h1>yes</h1>
+        <h1>{isGoingOut ? "Yes" : "No"}</h1>
       </div>
     </div>
   );
