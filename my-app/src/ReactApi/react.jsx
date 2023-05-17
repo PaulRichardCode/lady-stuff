6358e76442cd13fc55621d9c5ba6368573426125
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-17 13:53:59 +0100

    s

---
+++ my-app/src/ReactApi/react.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Ronke = () => {
+  return (
+    <>
+      <h1>list of Api</h1>
+      <ul>
+        <li>hey</li>
+        <li>ooh</li>
+      </ul>
+    </>
+  );
+};
+
+export default Ronke;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,5 +5,6 @@ import App from "./component/app.js";
 import Main from "./MOUNT/Main.js";
 import Items from "./Classwork/items.jsx";
 import Apple from "./Classwork/app.js";
+import Api from "./ReactApi/react.jsx";
 
-ReactDOM.render(<Apple />, document.querySelector("#root"));
+ReactDOM.render(<Api />, document.querySelector("#root"));
