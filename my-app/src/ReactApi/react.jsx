e9082fa0a196aa86986ca67674b33d383b22a3f5
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-17 14:23:40 +0100

    ok

--- my-app/src/ReactApi/react.jsx
+++ my-app/src/ReactApi/react.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 
 const Ronke = () => {
+  const [data, setData] = React.useState({});
+  const [count, setCount] = React.useState(0);
+  //get the data to Fetch
+  //save te data to state
+
+  fetch("http://swapi.dev/api/people/1")
+    .then((res) => res.json())
+    .then((data) => setData(data));
+
   return (
     <>
-      <h1>list of Api</h1>
-      <ul>
-        <li>hey</li>
-        <li>ooh</li>
-      </ul>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h1>The count is {count}</h1>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Add item
+      </button>
     </>
   );
 };
