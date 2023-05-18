0b67d968afba0a0b38670bdc30501437e3a54802
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-18 15:32:36 +0100

    okay

--- my-app/src/Classwork/app.js
+++ my-app/src/Classwork/app.js
@@ -1,5 +1,4 @@
 import React from "react";
-import data from "../MOUNT/data";
 
 export default function App() {
   /*   const [isGoingOut, setIsGoingOut] = React.useState(true);
@@ -19,45 +18,31 @@ export default function App() {
 
   // const thingsElements = thingsArray.map((thing) => <p key={thing}>{thing}</p>);
 
-  // const [thingsArray, setThingsArray] = React.useState([
-  //   "Richard 1",
-  //   "Richard 2",
-  // ]);
-
-  // function addItem() {
-  //   setThingsArray((prevThings) => [
-  //     ...prevThings,
-  //     `Richard ${prevThings.length + 1}`,
-  //   ]);
-  // }
-
-  // function reset() {
-  //   setThingsArray((prevThings) => [prevThings.shift()]);
-  // }
-
-  // const thingsElements = thingsArray.map((things) => <p>{things}</p>);
-
-  // return (
-  //   <div className="main">
-  //     <button onClick={addItem}>Add items:</button>
-  //     <div className="div">
-  //       <h1>{thingsElements}</h1>
-  //     </div>
-  //     <button onClick={reset}>reset</button>
-  //   </div>  );
-
-  const [item, setItem] = React.useState(["Things 1", "Things 2"]);
-  console.log(item);
+  const [thingsArray, setThingsArray] = React.useState([
+    "Richard 1",
+    "Richard 2",
+  ]);
 
   function addItem() {
-    setItem((data) => [...data, `Things ${data.length + 1}`]);
+    setThingsArray((prevThings) => [
+      ...prevThings,
+      `Richard ${prevThings.length + 1}`,
+    ]);
   }
 
-  const thingsElement = item.map((items) => <p>{items}</p>);
+  function reset() {
+    setThingsArray((prevThings) => [prevThings.shift()]);
+  }
+
+  const thingsElements = thingsArray.map((things) => <p>{things}</p>);
+
   return (
-    <div>
-      <button onClick={addItem}>Add items</button>
-      <h1>{thingsElement}</h1>
+    <div className="main">
+      <button onClick={addItem}>Add items:</button>
+      <div className="div">
+        <h1>{thingsElements}</h1>
+      </div>
+      <button onClick={reset}>reset</button>
     </div>
   );
 }
