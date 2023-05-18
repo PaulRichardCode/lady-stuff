6c7ca79fee2143a59190b6e24bfb4c385686ebc4
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-18 15:26:20 +0100

    d

--- my-app/src/Classwork/app.js
+++ my-app/src/Classwork/app.js
@@ -18,31 +18,43 @@ export default function App() {
 
   // const thingsElements = thingsArray.map((thing) => <p key={thing}>{thing}</p>);
 
-  const [thingsArray, setThingsArray] = React.useState([
-    "Richard 1",
-    "Richard 2",
-  ]);
+  // const [thingsArray, setThingsArray] = React.useState([
+  //   "Richard 1",
+  //   "Richard 2",
+  // ]);
 
-  function addItem() {
-    setThingsArray((prevThings) => [
-      ...prevThings,
-      `Richard ${prevThings.length + 1}`,
-    ]);
-  }
+  // function addItem() {
+  //   setThingsArray((prevThings) => [
+  //     ...prevThings,
+  //     `Richard ${prevThings.length + 1}`,
+  //   ]);
+  // }
 
-  function reset() {
-    setThingsArray((prevThings) => [prevThings.shift()]);
-  }
+  // function reset() {
+  //   setThingsArray((prevThings) => [prevThings.shift()]);
+  // }
 
-  const thingsElements = thingsArray.map((things) => <p>{things}</p>);
+  // const thingsElements = thingsArray.map((things) => <p>{things}</p>);
 
+  // return (
+  //   <div className="main">
+  //     <button onClick={addItem}>Add items:</button>
+  //     <div className="div">
+  //       <h1>{thingsElements}</h1>
+  //     </div>
+  //     <button onClick={reset}>reset</button>
+  //   </div>  );
+
+  const [item, setData] = React.useState(["Things 1", "Things 2"]);
+  console.log(item);
+
+  function addItem() {}
+
+  const thingsElement = item.map((items) => <p>{items}</p>);
   return (
-    <div className="main">
-      <button onClick={addItem}>Add items:</button>
-      <div className="div">
-        <h1>{thingsElements}</h1>
-      </div>
-      <button onClick={reset}>reset</button>
+    <div>
+      <button onClick={addItem}>Add items</button>
+      <h1>{thingsElement}</h1>
     </div>
   );
 }
